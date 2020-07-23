7b382fa3c20fb8dcb2d7434d2f715e4721d4b2cf
kvarlamov <k.varlamov88@yandex.ru>
2020-07-23 21:38:27 +0300

    7. Authorization - using service = Google Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyBoGxDNn1DJgXA7gg3MeJInuTwThvHzEtA"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyBoGxDNn1DJgXA7gg3MeJInuTwThvHzEtA"
 };
 
 /*
