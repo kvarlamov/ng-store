8744ae6bcf1afa79a54abdaabfb71288daf6eb34
kvarlamov <k.varlamov88@yandex.ru>
2020-07-27 21:49:02 +0300

    8. Work with token

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,37 @@ export class AuthService {
 
   login( User ) {
     return this.httpClient.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, User)
+      .pipe(
+        tap(this.setToken)
+      );
   }
+
+  private setToken(response) {
+    if (response) {
+      const expData = new Date(new Date().getTime() + response.expiresIn * 1000)
+      localStorage.setItem('fb-token-exp', expData.toString())
+      localStorage.setItem('fb-token', response.idToken);
+    } else {
+      localStorage.clear();
+    }
+  }
+
+  get Token()  {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
+  }
+
+  logout() {
+    this.setToken(null);
+  }
+
+  isAuthenticated() {
+    return !!this.Token;
+  }
+
+
 }
