d853f240a6dbfcf716d2cab9cbf2d5958666d13d
kvarlamov <k.varlamov88@yandex.ru>
2020-08-06 22:57:39 +0300

    13. product page

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {map} from 'rxjs/operators';
-import {FbResponse} from './interfaces';
+import {FbResponse, Product} from './interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -33,4 +33,15 @@ export class ProductService {
           }));
       }));
   }
+
+  getById(id) {
+    return this.http.get(`${environment.fbDbUrl}/products/${id}.json`)
+      .pipe( map ( (res: Product) => {
+        return {
+          ...res,
+          id,
+          date: new Date(res.date)
+          }
+        }))
+    }
 }
